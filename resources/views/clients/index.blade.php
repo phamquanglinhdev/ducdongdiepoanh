b8e6d5966f4fa6eafb7c9bfc19e423decdc8aab8
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-06-16 11:35:35 +0700

    add video

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -225,6 +225,21 @@
             </div>
         </div>
     </section>
+    <section class="videos mt-lg-5 mt-3 pb-5">
+        <div class="container">
+            <h1 class="text-main text-center font-weight-bold">VIDEO NỔI BẬT</h1>
+            <div class="text-center"><img src="{{asset("assets/images/products-sticker.png")}}" class="img-fluid py-2"
+                                          alt=""></div>
+            <div class="row m-0">
+                <div class="col-md-6 col-12">
+                    <iframe class="rounded" width="100%" height="300px" src="https://www.youtube.com/embed/I-NW0wMhhDA"></iframe>
+                </div>
+                <div class="col-md-6 col-12">
+                    <iframe  class="rounded" width="100%" height="300px" src="https://www.youtube.com/embed/Lfri2bVjo44"></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
     <!-- bat dau phan video  -->
 {{--    <section class="introduce-product">--}}
 {{--        <div class="container p-5 m-auto">--}}
